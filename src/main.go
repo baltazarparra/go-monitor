a89d35637398b1901ecfc288efe8b508bf3ae2cd
Baltazar Parra <btparra@esapiens.com.br>
2019-02-07 06:24:14 -0200

    Verificando status code de sites

--- src/main.go
+++ src/main.go
@@ -1,16 +1,84 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("1- Iniciar monitoramento")
-	fmt.Println("2- Exibir logs")
-	fmt.Println("0- Sair")
+
+	for {
+		menu()
+
+		switch userEntry() {
+		case 1:
+			monitoring()
+		case 2:
+			logging()
+		case 3:
+			entrySite()
+		case 0:
+			fmt.Println("Saindo...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não reconheço esse comando")
+		}
+	}
+
+}
+
+func menu() {
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("3 - Monitorar outro site")
+	fmt.Println("0 - Sair")
 	fmt.Println("...")
+}
 
+func userEntry() int {
 	var comando int
-
 	fmt.Scan(&comando)
+	return comando
+}
 
-	fmt.Println("Comando escolhido:", comando)
+func monitoring() {
+	fmt.Println("Monitorando...")
+
+	sites := []string{"https://baltazarparra.github.io/error", "https://google.com", "https://twitter.com", "https://youtube.com"}
+
+	for _, site := range sites {
+		isUp(site)
+	}
+}
+
+func logging() {
+	fmt.Println("Exibindo logs...")
+}
+
+func entrySite() {
+	fmt.Println("Digite o site que deseja monitorar...")
+	var site string
+	fmt.Scan(&site)
+	response, _ := http.Get(site)
+
+	if response.StatusCode == 200 {
+		fmt.Println(site, "está de pé!")
+	}
+
+	if response.StatusCode != 200 {
+		fmt.Println(site, "está com problemas para carregar, Status:", response.StatusCode)
+	}
+}
+
+func isUp(site string) {
+	response, _ := http.Get(site)
+
+	if response.StatusCode == 200 {
+		fmt.Println(site, "está de pé!")
+	}
+
+	if response.StatusCode != 200 {
+		fmt.Println(site, "não com problema, Status:", response.StatusCode)
+	}
 }
